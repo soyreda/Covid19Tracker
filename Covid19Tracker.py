ef5ae480c60e892b3443d65adfb186d0b3e67dbf
Re Da <50604310+bitz3r@users.noreply.github.com>
2020-04-07 11:54:25 +0100

    Add files via upload

---
+++ Covid19Tracker.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import sys
+
+
+
+url = 'https://www.worldometers.info/coronavirus/'
+response = requests.get(url)
+source = response.text
+
+
+soup = BeautifulSoup(source, 'html.parser')
+
+titles = []
+numbers = []
+countries = []
+
+#Looping through covid 19 cases over the world
+for number in soup.find_all('div', id='maincounter-wrap'):
+    titles.append(number.h1.text)
+    numbers.append(number.div.span.text)
+
+i = 0
+for title in titles:
+    print("{0} {1}".format(title, numbers[i]))
+    i += 1
+
+
+
